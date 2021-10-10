46b2290df7ee411d0279a52c40ce13670875e89c
xjarlie <xjarlie@gmail.com>
2021-10-10 14:13:11 +0000

    issue #10

--- public/chat.js
+++ public/chat.js
@@ -148,7 +148,7 @@ class Message {
         } else if (this.timestamp == 1) {
             timeString = '';
         } else {
-            timeString = this.date.toLocaleDateString('en-GB', { timeZone: 'Asia/Seoul' });
+            timeString = this.date.toLocaleDateString('en-GB', { timeZone: 'Asia/Seoul', year: '2-digit', month: '2-digit', day: '2-digit' });
         }
 
         const messageDiv = document.createElement('div');
@@ -170,7 +170,7 @@ class Message {
 
         const textSpan = document.createElement('span');
         textSpan.classList.add('text');
-        textSpan.textContent = this.text;
+        textSpan.innerHTML = linkify(this.text);
 
         messageDiv.append(timeSpan);
         messageDiv.append(authorSpan);
@@ -185,8 +185,14 @@ class Message {
 }
 
 let chatDiv = document.getElementById('viewChat');
-const placeholderMessage = new Message('', '', '', 1, chatDiv);
-placeholderMessage.init();
+for (let i = 1; i < 3; i++) {
+    const placeholderMessage = document.createElement('div');
+    placeholderMessage.classList.add('chatMessage');
+    placeholderMessage.classList.add('card-panel');
+    placeholderMessage.style.pointerEvents = 'none';
+    chatDiv.prepend(placeholderMessage);
+}
+
 
 let scrolled = false;
 function updateScroll() {
@@ -218,3 +224,20 @@ document.addEventListener('DOMContentLoaded', function () {
     var instances2 = M.Tooltip.init(elems2, {});
 });
 
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3, replacePattern4;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
